62fe8d04d18b895e9330b4a1e81aab9ce06238b0
LemonadeCodeGirl <42980263+LemonadeCodeGirl@users.noreply.github.com>
2023-11-07 16:25:46 -0500

    Added WT Forms for easier access

--- core/views.py
+++ core/views.py
@@ -1,14 +1,34 @@
 from core import app
 from flask import Flask, render_template
 
+from flask import Flask, render_template, redirect, url_for, request
+from flask_bootstrap import Bootstrap5
+
+from flask_wtf import FlaskForm, CSRFProtect
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired, Length
+
+import secrets
+foo = secrets.token_urlsafe(16)
+app.secret_key = foo
+
 import oracledb
 
 currentQuery = 1
+thingName = ""
 
 connection = oracledb.connect(user="obisset", password="B69SI5SeQAugPl6lHcd5YrL4", dsn="oracle.cise.ufl.edu:1521/orcl")
-
-
 cursor = connection.cursor()
+
+
+# Bootstrap-Flask requires this line
+bootstrap = Bootstrap5(app)
+# Flask-WTF requires this line
+csrf = CSRFProtect(app)
+
+class NameForm(FlaskForm):
+    name = StringField('Which actor is your favorite?')
+    submit = SubmitField('Submit')
 
 @app.route('/')
 def index():
@@ -25,9 +45,29 @@ def about():
 def querySelector():
     return render_template('querySelector.html')
 
-@app.route('/query1')
+@app.route('/query1', methods=['GET', 'POST'])
 def queryOne():
-    return render_template('query1.html')
+    form = NameForm()
+    message = ""
+
+    if form.validate_on_submit():
+        name = form.name.data
+        print(name)
+    #     if name.lower() in names:
+    #         # empty the form field
+    #         form.name.data = ""
+    #         id = get_id(ACTORS, name)
+    #         # redirect the browser to another route and template
+
+        global thingName
+        thingName = name
+        print(thingName)
+        return redirect( url_for('results', thing=thingName) )
+    #     else:
+    #         message = "That actor is not in our database."
+    # # return render_template('index.html', names=names, form=form, message=message)
+
+    return render_template('query1.html', messages = "", form=form)
 
 
 def test():
@@ -41,4 +81,6 @@ def test():
 
 @app.route('/results')
 def results():
-    return render_template('results.html', number=currentQuery, results=test())
+    thing = request.args.get('name')
+    print(thingName)
+    return render_template('results.html', number=currentQuery, results=test(), thing=thingName)
